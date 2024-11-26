5993a8fa81b1a6fe2e81ca019ab57331a2cdcf51
Helavista27 <d.Gorshkov@hightech-plant.ru>
2024-11-26 09:51:38 +0300

    2

--- test_pt1.py
+++ test_pt1.py
@@ -2,8 +2,8 @@ import re
 from playwright.sync_api import Playwright, sync_playwright, expect
 import time
 
-def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=True)
+def test_add_pt1(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.goto("https://demo.playwright.dev/todomvc/#/")
